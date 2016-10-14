6c120c3e0b4cc58a896547d12a8f78713c1503e9
caleblareau <caleblareau@g.harvard.edu>
2016-10-14 01:12:05 -0400

    another tweak for travis

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,9 +13,8 @@ def file_checksums_equal(file1, file2):
 def test_preproc_run():
     runner = CliRunner()
     result = runner.invoke(cli.main, ['--out', 'output1',  'example.yaml'])
-    assert not result.exception
     assert result.exit_code == 0
 
-def test_reads_for_peak_calling():
+def test_loops_output():
 	assert file_checksums_equal('correct_output/co.intra.loop_counts.bedpe', 'output1/test_sample1.intra.loop_counts.bedpe')
 	assert file_checksums_equal('correct_output/co.intra.loop_counts.bedpe', 'output1/test_sample2.intra.loop_counts.bedpe')
