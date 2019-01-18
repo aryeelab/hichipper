4d5bbb2a06a3a7388b41d25c2e5b78774dd694cf
caleblareau <caleblareau@g.harvard.edu>
2019-01-18 12:56:57 -0500

    version bump

--- Authors.rst
+++ Authors.rst
@@ -16,5 +16,5 @@ Contributors
 ------------
 
 * Alejandro Reyes <alejandro.reyes.ds@gmail.com>
-
+* Afif Elghraoui
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click', 'PyYAML', 'Numpy', 'MACS2', 'whichcraft']
 
 setup(
     name='hichipper',
-    version='0.7.3',
+    version='0.7.5',
     url='https://github.com/aryeelab/hichipper',
     license='MIT',
     author='Caleb Lareau and Martin Aryee',
